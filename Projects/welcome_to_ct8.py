d9e64072c143ed20bebc9e4a2a4a50a91c4ad319
bird-ui <ethanvoboril@seattleacademy.org>
2024-12-04 20:04:27 +0000

    secont day of computer

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -1,28 +1,21 @@
+
 ###############################################
 ### SETUP ###
+from tkinter import Variable
 import codesters
 from codesters import StageClass
 stage = StageClass()
 ###############################################
 
 
-stage.set_background("fall")
+stage.set_background("moon")
 mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
+mySprite.say("nuh uh!")
 
 
-print("Welcome to CT8!")
-print("Great job correctly setting up this codespace")
-print("We will be using this site for the majority of the projects for this class")
-print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
-input("\n---Press Enter when you have bookmarked the page---\n")
-
-
-print("\n\n")
-print("Now, try to view the display screen")
-print("To view the display screen:")
-print("\t1: click PORTS on the menu bar above this text")
-print("\t2: move your mouse over the words in blue that start with https://")
-print("\t3: a few icons should appear - click the globe")
-print("\t4: a new tab will open - click CONNECT")
-print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
+print("\n\nWhen you have found the bodies, click here, then use CTRL C to end the program\n\n")
+print("this is the new last instruction stupid")
+x=mySprite2=codesters.Sprite("baseball",-250,245)
+mySprite2=codesters.Sprite("baseball",-250,245)
+mySprite2=codesters.Sprite("baseball",-250,245)
+mySprite2=codesters.Sprite("baseball",-250,245)
