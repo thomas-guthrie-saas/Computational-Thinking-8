0c777fab6bd7c24f921f729970ca1984b654604a
rainbowcupcakes6000 <jackgregersen@seattleacademy.org>
2024-12-04 20:04:27 +0000

    second day of CT8

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -6,23 +6,8 @@ stage = StageClass()
 ###############################################
 
 
-stage.set_background("fall")
-mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
-
-
-print("Welcome to CT8!")
-print("Great job correctly setting up this codespace")
-print("We will be using this site for the majority of the projects for this class")
-print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
-input("\n---Press Enter when you have bookmarked the page---\n")
-
-
-print("\n\n")
-print("Now, try to view the display screen")
-print("To view the display screen:")
-print("\t1: click PORTS on the menu bar above this text")
-print("\t2: move your mouse over the words in blue that start with https://")
-print("\t3: a few icons should appear - click the globe")
-print("\t4: a new tab will open - click CONNECT")
-print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
+stage.set_background("drawbridge")
+mySprite = codesters.Sprite("cool_dog")
+mySprite.say("Bow wow young blud")
+mySprite2 = codesters.Sprite("fox",150,-50)
+mySprite2.say("What does the fox say?")
