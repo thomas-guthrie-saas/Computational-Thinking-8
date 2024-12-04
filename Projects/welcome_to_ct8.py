a27aac3949a7c4721376575d068a808872122874
ZoeyJJ2011 <zoeyjefferson@seattleacademy.org>
2024-12-04 20:15:59 +0000

    day 2

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -6,7 +6,7 @@ stage = StageClass()
 ###############################################
 
 
-stage.set_background("summ")
+stage.set_background("summer")
 mySprite = codesters.Sprite("fish")
 mySprite.say("Yo what's up")
 mySprite2 = codesters.Sprite("nothing",0,0)
