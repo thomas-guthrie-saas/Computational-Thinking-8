299f005bf9e3b864226b0e700027791c5d79cad4
dulllnes <agnesdull@seattleacademy.org>
2024-12-05 17:04:14 +0000

    day2 of ct8

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -6,23 +6,18 @@ stage = StageClass()
 ###############################################
 
 
-stage.set_background("fall")
-mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
+stage.set_background("spring")
 
 
-print("Welcome to CT8!")
-print("Great job correctly setting up this codespace")
-print("We will be using this site for the majority of the projects for this class")
-print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
-input("\n---Press Enter when you have bookmarked the page---\n")
 
 
-print("\n\n")
-print("Now, try to view the display screen")
-print("To view the display screen:")
-print("\t1: click PORTS on the menu bar above this text")
-print("\t2: move your mouse over the words in blue that start with https://")
-print("\t3: a few icons should appear - click the globe")
-print("\t4: a new tab will open - click CONNECT")
+
 print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
+print("This is the new last instruction")
+mySprite("Lloyd_Garmadon",0,0)
+mySprite2 = codesters.Sprite("cat2",100,100)
+mySprite3 = codesters.Sprite("kitten",200,-200)
+mySprite4 = codesters.Sprite("character1",-20,-50)
+mySprite4.say("hi ananya")
+mySprite2.say("meow.")
+mySprite3.say("meow.")
