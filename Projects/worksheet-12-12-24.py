1a79852f8683b44ccf14ff68ab4806a07da8ff1b
thomas-guthrie-saas <thomasguthrie@seattleacademy.org>
2024-12-12 18:48:21 +0000

    g

--- Projects/worksheet-12-12-24.py
+++ Projects/worksheet-12-12-24.py
@@ -5,9 +5,9 @@ while True:
     print("-------------------------")
     word = input("haha wat your grama likey\n\n")
     print("")
-    if "g" in word:
-        print(f"grammy no likey {word}!11!")
+    if "g" not in word:
+        print(f"grammy no likey '{word}'!11!")
         wait.sleep(.3)
     else:
-        print(f"grandma likes {word}")
+        print(f"grandma likey '{word}'!11")
         wait.sleep(.3)
