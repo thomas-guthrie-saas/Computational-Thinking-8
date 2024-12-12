9dd9b005a1f54a6977c76fb6123065628778e438
thomas-guthrie-saas <thomasguthrie@seattleacademy.org>
2024-12-12 18:35:57 +0000

    worksheet

---
+++ Projects/worksheet-12-12-24.py
@@ -0,0 +1,14 @@
+import time as wait
+
+while True:
+    print("")
+    word = input("haha wat your grama likey\n")
+    print("")
+    if "t" in word:
+        print(f"grammy no likey {word}!11!")
+    else:
+        print(f"grandma likes {word}")
+
+        print("")
+        wait.sleep(1)
+        
